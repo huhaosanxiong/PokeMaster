707ddf57f08541ab800d60451de0674c84e6eaaa
胡浩三雄 <huhsxxx@gmail.com>
2020-07-30 12:09:00 +0800

    Initial Commit

---
+++ PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by huhsx on 2020/7/30.
+//  Copyright © 2020 huhsx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
